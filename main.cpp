3524626b7088ea2a3f596d7cf1e7b30ecb42e91d
qlzhai <qlzhai2022@163.com>
2024-07-10 11:03:54 +0800

    message

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 
 using namespace std;
 
+//本文件用于测试和学习git
 int main() {
     cout << "hello" << endl;
+    cout << "world" << endl;
     system("pause");
     return 0; // End the program with a return code of 0
 }

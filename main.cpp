3cf5b9b895dd3cdac9fd65872e5fd28056276508
qlzhai <qlzhai2022@163.com>
2024-07-10 11:15:20 +0800

    add some functions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,34 @@
 
 using namespace std;
 
+void addFun(){
+    int a = 10;
+    int b = 20;
+    cout << "The sum is: " << a + b << endl;    
+}
+
+void minFun(){
+    int a = 10;
+    int b = 20;
+    cout << "The difference is: " << a - b << endl;
+}
+
+void mulFun(){
+    int a = 10;
+    int b = 20;
+    cout << "The product is: " << a * b << endl;    
+}
+
+void divFun(){
+    int a = 10;
+    int b = 20;
+    if(b == 0){
+        cout << "Error: Division by zero is not allowed." << endl;
+    } else {
+        cout << "The quotient is: " << a / b << endl;
+    }   
+}
+
 //本文件用于测试和学习git
 int main() {
     cout << "hello" << endl;
